8e0a80bfdd86d7b75c7038ae5a0eeb94a2ebcef0
darilugh2 <yanminwai122@gmail.com>
2024-11-02 11:45:19 +0900

    Completed Introsection

---
+++ Week2/first-app/app/component/IntroSection.jsx
@@ -0,0 +1,34 @@
+import React from 'react' //op
+
+const IntroSection = () => {
+
+    const name = String("Yinlin"); // constant declaration (explicit String)
+    let age = Number(12.2); // variable declaration (expllicit Number)
+
+    return  (
+    <div className="h-[100vh] w-screen flex justify-center items-center">
+        <div className="w-full h-full px-24 flex flex-col items-center justify-center">
+        <p className="text-center text-2xl">Hello, my name is {name}.</p>
+        <br />
+        <p className="text-center text-sm">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam
+          aspernatur, cumque labore nemo quos voluptates. Lorem ipsum dolor sit
+          amet consectetur adipisicing elit. Quibusdam aspernatur, cumque labore
+          nemo quos voluptates. Lorem ipsum dolor sit amet consectetur
+          adipisicing elit. Quibusdam aspernatur, cumque labore nemo quos
+          voluptates. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Quibusdam aspernatur, cumque labore nemo quos voluptates.
+        </p>
+      </div>
+      <div className="w-full h-full flex flex-col items-center justify-center">
+        <img src="yinlin.jpg" className="w-[600px] h-[600px]"/>
+        <p>
+          Hello My name is {name} and I am {age} years old.
+        </p>
+      </div>
+    </div>   
+    ) 
+    
+}
+
+export default IntroSection;
---
+++ Week2/first-app/app/component/ProfileSection.jsx
@@ -0,0 +1,14 @@
+import React from 'react'   //optional
+
+const ProfileSection = () => {
+
+return(
+
+    <>Helloo</>
+
+
+)
+
+}
+
+export default ProfileSection;
